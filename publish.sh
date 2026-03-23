805612c325ff1ca7ff7003f1fd919fec355f73a0
nagarajan <nag.rajan@gmail.com>
2026-03-23 00:39:31 -0700

    no build on publish

--- publish.sh
+++ publish.sh
@@ -1,7 +1,6 @@
 cd ~/dev/motleytech-astro
 git pull
 echo "Pulled latest changes"
-npm run build
 mkdir -p /var/www/motleytech/html
 rm -rf /var/www/motleytech/html/astro
 cp -r dist /var/www/motleytech/html/astro
